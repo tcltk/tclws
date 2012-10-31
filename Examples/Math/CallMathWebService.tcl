a40fe7437a097ed574ba3afff111f735c1662781
gerald <Gerald.Lester@gmail.com>
2012-10-31 06:08:25 +0000

    Fix for: [9143f6d463[ -- Missing square brackets in Examples/Math/CallMathWebService.tcl

--- Examples/Math/CallMathWebService.tcl
+++ Examples/Math/CallMathWebService.tcl
@@ -122,5 +122,5 @@ dict set input polynomial $polynomial
 ## Call service
 ##
 puts stdout "Calling EvaluatePolynomial with {$input}"
-set resultsDict ::WS::Client::DoCall wsMathExample EvaluatePolynomial $input
+set resultsDict [::WS::Client::DoCall wsMathExample EvaluatePolynomial $input]
 puts stdout "Results are {$resultsDict}"
